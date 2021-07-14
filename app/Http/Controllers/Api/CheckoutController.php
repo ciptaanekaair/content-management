bf492b8005c6d912be9fefe70d1d194b23fa17d7
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-07-14 16:26:36 +0700

    Penambahan Fungsi checkout v.2

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Auth;
 use Validator;
 use Carbon\Carbon;
+use App\Models\Product;
 use App\Models\TransactionTemporary;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
@@ -16,7 +17,25 @@ class CheckoutController extends Controller
 {
     public function vcrValidasi($kode)
     {
-        $vcr = Voucher::where('voucher_code', $kode)->first();
+        $vcr = Voucher::where('voucher_code', $kode)->where('status', '!=', 0)->first();
+
+        if (!$vcr) {
+            return false;
+        }
+
+        $dateNow = Carbon::today();
+        $dateEnd = Carbon::createFromFormat('Y-m-d H:i:s', $vcr->voucher_end);
+        $check   = $dateEnd->gt($dateNow);
+
+        $response = ['voucher_id' => $vcr->id, 'voucher_price' => $vcr->voucher_price];
+
+        return $response;
+    }
+
+    // validasi berdasarkan ID
+    public function vcrValidasiID($id)
+    {
+        $vcr = Voucher::where('id', $id)->first();
 
         if (!$vcr) {
             return false;
@@ -52,11 +71,11 @@ class CheckoutController extends Controller
 
         if ($lProduct->isEmpty()) {
             $response = [
-                'success' => true,
+                'error' => true,
                 'message' => 'Anda belum memilih product. Silahkan pilih product di halaman product.'
             ];
 
-            return response($response, 200);
+            return response($response, 403);
         }
 
         // devine variable baru
@@ -95,88 +114,128 @@ class CheckoutController extends Controller
         $transaksi->price_after_discount = $p_after_discount;
         $transaksi->pajak_ppn            = $pajakPPN;
         $transaksi->sub_total_price      = $p_after_discount + $pajakPPN;
-        // $transaksi->save();
+        $transaksi->save();
 
-        $response = ['data' => $transaksi];
+        $transaksi_id = $transaksi->id;
 
+        foreach($lProduct as $item) {
+            TransactionDetail::create([
+                'transactions_id' => $transaksi_id,
+                'product_id'      => $item->product_id,
+                'qty'             => $item->qty,
+                'total_price'     => $item->total_price,
+            ]);
 
-        return response($response, 200);
-    }
+            $product = Product::findOrFail($item->product_id);
+            $product->product_stock -= $item->qty;
+            $product->update();
 
-    /**
-     * Checkout seluruh barang di keranjang
-     */ 
-    public function checkouta(Request $request)
-    {
-        $rules = [
-            'user_id' => 'required',
-        ];
-
-        $pesan = [
-            'user_id.required' => 'Session anda telah berakhir. Login kembali agar dapat melanjutkan transaksi.'
-        ];
-
-        $validasi = Validator::make($request->all(), $rules, $pesan);
-
-        if ($validasi->fails()) {
-            return response([
-                'error' => true,
-                'message' => $validasi->errors()
-            ], 401);
+            $item->delete();
         }
-
-        $lProduct  = TransactionTemporary::where('user_id', Auth::user()->id)->get();
-        $p_on_cart = $lProduct->count();
-
-        if ($lProduct->isEmpty()) {
-            $response = [
-                'success' => true,
-                'message' => 'Anda belum memilih product. Silahkan pilih product di halaman product.'
-            ];
-
-            return response($response, 200);
-        }
-
-        $total_price = Auth::user()->countTotalPrice();
-        $pajakPPN    = $total_price * 0.1;
-        $discount    = 0;
-        $code_vcr    = $request->voucher_code;
-
-        // if ($request->voucher_id != '') {
-        //     $voucher  = Voucher::findOrFail($request->voucher_id);
-        //     $discount = $voucher->voucher_price;
-        // }
-
-        // $transaksi = new Transaction;
-        // $transaksi->user_id          = Auth::user()->id;
-        // $transaksi->payment_code_id  = $request->payment_code_id == '' ? NULL : $request->payment_code_id;
-        // $transaksi->voucher_id       = $request->voucher_id == '' ? NULL : $request->voucher_id;
-        // $transaksi->transaction_code = 'TRN-'.date('ymd').rand('000', '999');
-        // $transaksi->transaction_date = date('Y-m-d');
-        // $transaksi->total_item       = Auth::user()->countQty();
-        // $transaksi->total_price      = $total_price;
-        // $transaksi->sub_total_price  = ($total_price + $pajakPPN) - $discount;
-        // $transaksi->save();
-
-        if ($code_vcr != '') {
-            $discount = $this->vcrValidasi($code_vcr);
-        }
-
-        // $response = [
-        //     'success' => true,
-        //     'message' => 'Berhasil checout keranjang belanja anda. Segera lakukan pembayaran, dan konfirmasi pembayaran.',
-        //     'data'    => $transaksi
-        // ];
 
         $response = [
-            'voucher_id'      => $discount->id,
-            'total_price'     => $total_price,
-            'discount'        => $discount,
-            'sub_total_price' => ($total_price + $pajakPPN) - $discount->voucher_price,
+            'success' => true,
+            'message' => 'Berhasil checkout! Silahkan lakukan pembayaran dan konfirmasikan pembayaran anda.',
+            'data'    => $transaksi
         ];
 
         return response($response, 200);
     }
+
+    // /**
+    //  * Checkout seluruh barang di keranjang
+    //  */ 
+    // public function checkout(Request $request)
+    // {
+    //     $rules = [
+    //         'user_id' => 'required',
+    //     ];
+
+    //     $validasi = Validator::make($request->all(), $rules);
+
+    //     if ($validasi->fails()) {
+    //         return response([
+    //             'error' => true,
+    //             'message' => $validasi->errors()
+    //         ], 401);
+    //     }
+
+    //     // Check ada produk dalam keranjang atau tidak
+    //     $lProduct  = TransactionTemporary::where('user_id', Auth::user()->id)->get();
+    //     $p_on_cart = $lProduct->count();
+
+    //     if ($lProduct->isEmpty()) {
+    //         $response = [
+    //             'error' => true,
+    //             'message' => 'Anda belum memilih product. Silahkan pilih product di halaman product.'
+    //         ];
+
+    //         return response($response, 403);
+    //     }
+
+    //     // devine variable baru
+    //     $total_price = Auth::user()->countTotalPrice();
+    //     $code_vcr    = $request->voucher_id;
+    //     $discount    = 0;
+
+    //     // buat transaksi baru
+    //     $transaksi          = new Transaction;
+    //     $transaksi->user_id = Auth::user()->id;
+
+    //     if (!empty($request->voucher_id)) {
+
+    //         $voucher  = $this->vcrValidasiID($code_vcr); // check validasi voucher.
+
+    //         // jika voucer ditemukan berdasarkan kode
+    //         if ($voucher != false) {
+
+    //             $discount = $voucher['voucher_price']; // variable discount
+
+    //             // siapkan data voucher_id untuk masuk kedalam database.
+    //             $transaksi->voucher_id = $voucher['voucher_id'];
+    //         }
+
+    //     }
+
+    //     // buat perhitungan
+    //     $p_after_discount = $total_price - $discount;
+    //     $pajakPPN         = $p_after_discount * 0.1;
+
+    //     $transaksi->transaction_code     = 'TRN-'.date('ymd').rand('000', '999');
+    //     $transaksi->transaction_date     = date('Y-m-d');
+    //     $transaksi->total_item           = Auth::user()->countQty();
+    //     $transaksi->total_price          = $total_price;
+    //     $transaksi->discount             = $discount;
+    //     $transaksi->price_after_discount = $p_after_discount;
+    //     $transaksi->pajak_ppn            = $pajakPPN;
+    //     $transaksi->sub_total_price      = $p_after_discount + $pajakPPN;
+    //     $transaksi->save();
+
+    //     $transaksi_id = $transaksi->id;
+
+    //     foreach($lProduct as $item) {
+    //         TransactionDetail::create([
+    //             'transactions_id' => $transaksi_id,
+    //             'product_id'      => $item->product_id,
+    //             'qty'             => $item->qty,
+    //             'total_price'     => $item->total_price,
+    //         ]);
+
+    //         $product = Product::findOrFail($item->product_id);
+    //         $product->product_stock -= $item->qty;
+    //         $product->update();
+
+    //         $item->delete();
+    //     }
+
+    //     $response = [
+    //         'success' => true,
+    //         'message' => 'Berhasil checkout! Silahkan lakukan pembayaran dan konfirmasikan pembayaran anda.',
+    //         'data'    => $transaksi
+    //     ];
+
+    //     return response($response, 200);
+    // }
 
     public function validasiVoucher(Request $request)
     {
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -24,7 +24,7 @@ class ProductSeeder extends Seeder
             'product_images'      => 'product-images/0d98dad3689d87a13bb0fef1e18629e0.jpg',
             'product_price'       => '29000000',
             'product_commision'   => '10',
-            'product_stock'       => 999,
+            'product_stock'       => 1000,
             'status'              => 1
         ]);
         Product::create([
@@ -34,7 +34,7 @@ class ProductSeeder extends Seeder
             'slug'                => 'produk-tester-2',
             'product_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.',
             'product_images'      => 'product-images/4210ca147391331cc27600f77529ae94.jpg',
-            'product_price'       => '36000000',
+            'product_price'       => '360000',
             'product_commision'   => '10',
             'product_stock'       => 100,
             'status'              => 1
@@ -46,9 +46,22 @@ class ProductSeeder extends Seeder
             'slug'                => 'produk-tester-10',
             'product_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.',
             'product_images'      => 'product-images/cf682b73be1afad47d0f32559ac34627.jpg',
-            'product_price'       => '26000000',
+            'product_price'       => '2600000',
             'product_commision'   => '10',
             'product_stock'       => 200,
+            'status'              => 1
+        ]);
+
+        Product::create([
+            'product_category_id' => '2',
+            'product_code'        => '01BB0002',
+            'product_name'        => 'Produk Tester 11',
+            'slug'                => 'produk-tester-11',
+            'product_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do iusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'product_images'      => 'product-images/cf682b73be1afad47d0f32559ac34627.jpg',
+            'product_price'       => '750000',
+            'product_commision'   => '10',
+            'product_stock'       => 1000,
             'status'              => 1
         ]);
 
