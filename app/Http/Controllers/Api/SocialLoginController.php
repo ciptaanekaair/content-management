3084883b8662e709e1cb322c0188721dafe0ecc2
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-07-21 09:22:21 +0700

    fixing again

--- app/Http/Controllers/Api/SocialLoginController.php
+++ app/Http/Controllers/Api/SocialLoginController.php
@@ -27,30 +27,30 @@ class SocialLoginController extends Controller
 
         if (!empty($cek_user)) {
             $simpan = $serviceUser->getAvatar->storeAs('profile-photos', 'public');
+            
+            $token = $cek_user->createToken('usertoken')->plainTextToken;
 
-            $user = User::create([
+            return response([
+                'success' => true,
+                'message' => 'Berhasil melakukan autentikasi.',
+                'data'    => $cek_user
+            ]);
+        }
+
+        $user = User::create([
                 'name'               => $serviceUser->getName,
                 'email'              => $serviceUser->getEmail,
                 'email_verified_at'  => date('Y-m-d H:i:s'),
                 'password'           => Hash::make(rand('111111', '999999')),
                 'profile_photo_path' => $simpan
             ]);
-            
-            $token = $user->createToken('usertoken')->plainTextToken;
 
-            return response([
-                'success' => true,
-                'message' => 'Berhasil melakukan autentikasi.',
-                'data'    => $user
-            ]);
-        }
-
-        $token = $cek_user->createToken('usertoken')->plainTextToken;
+        $token = $user->createToken('usertoken')->plainTextToken;
 
         return response([
             'success' => true,
             'message' => 'Berhasil melakukan autentikasi.',
-            'data'    => $cek_user
+            'data'    => $user
         ]);
     }
 }
