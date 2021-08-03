de83c0269c8f901b8fe8c9c2945b3a03975334dc
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-08-03 08:33:07 +0700

    penambahan library dan juga migration perubahan type column

--- app/Http/Controllers/Api/PaymentMethodController.php
+++ app/Http/Controllers/Api/PaymentMethodController.php
@@ -8,7 +8,8 @@ use App\Models\PaymentCode;
 
 class PaymentMethodController extends Controller
 {
-    public function index()
+    
+    public function store()
     {
         $payment = PaymentCode::where('status', '!=', 9)
                 ->orderBy('kode_pembayaran', 'ASC')
