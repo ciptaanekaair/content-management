31c6bbd00f20d0924326835574eab221a31a148b
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-08-09 11:38:32 +0700

    Fixing 3 mailing beli langsung [view]

--- resources/views/mailinvoice.blade.php
+++ resources/views/mailinvoice.blade.php
@@ -138,9 +138,9 @@
                           <tbody>
                             <tr>
                               <td align="left" style="font-size:0px;padding:10px 25px;padding-top:0;padding-bottom:0px;word-break:break-word;">
-                                <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:left;color:#1e40af;">
+                                <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;font-weight:bolder;line-height:1;text-align:left;color:#1e3a8a;">
                                   <center>
-                                    <img src="{{ asset('logo.png') }}" width="50%" alt="FilterPedia.co.id"><br>
+                                    <img src="{{ asset('images/Online report_Outline.svg') }}" width="50%" alt="FilterPedia.co.id"><br>
                                     FILTERPEDIA
                                   </center>
                                 </div>
