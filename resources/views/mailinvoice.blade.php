84cc3a9a744ea0ab16685034ee40f4acf8dd386d
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-08-09 11:54:38 +0700

    Fixing number_format mailing beli langsung [view]

--- resources/views/mailinvoice.blade.php
+++ resources/views/mailinvoice.blade.php
@@ -593,7 +593,7 @@
                               <tr>
                                 <td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:10px;word-break:break-word;">
                                   <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:left;color:#45474e;">
-                                    {{ $data->paymentMethod->cara_bayar }}
+                                    {!! $data->paymentMethod->cara_bayar !!}
                                   </div>
                                 </td>
                               </tr>
