e56ae89c41999ebf0c2ada80ee56f01cfcdf7bf6
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-08-10 09:05:56 +0700

    Penambahan dan finishing API request forget password

---
+++ app/Http/Controllers/Api/ForgetPasswordController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Models\PasswordReset;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ForgetPasswordMail;
+use Validator;
+use Session;
+use Hash;
+
+class ForgetPasswordController extends Controller
+{
+    public function forgetPassword(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email'
+        ];
+
+        $pesan = [
+            'email.required' => 'Form email harus diisi.',
+            'email.email'    => 'Form email harus diisi dengan email Anda.'
+        ];
+
+        $validasi = Validator::make($request->all(), $rules, $pesan);
+
+        if ($validasi->fails()) {
+            return response([
+                'error'   => true,
+                'message' => $validasi->errors()
+            ], 422);
+        }
+
+        $p_reset = PasswordReset::where('email', $request->email)->delete();
+
+        if ($this->checkEmailUser($request->email) == true) {
+            $generate_token     = rand(111111, 999999);
+
+            $data = User::where('email', $request->email)
+                    ->first();
+            $data->kode_rahasia = $generate_token;
+
+            $reset = new PasswordReset;
+            $reset->email      = $request->email;
+            $reset->token      = md5($generate_token);
+            $reset->created_at = date('Y-m-d H:i:s');
+            $reset->save();
+
+            Mail::to($request->email)->send(new ForgetPasswordMail($data));
+
+            return response([
+                'success' => true,
+                'message' => 'Berhasil melakukan permintaan perubahan password. Silahkan check email anda, untuk memasukan kode rahasia.'
+            ], 200);
+        }
+
+        return response([
+            'error'   => true,
+            'message' => 'Gagal, email Anda tidak terdaftar pada sistem kami. Silahkan lakukan pendaftaran akun baru.'
+        ], 401);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $rules = [
+            'token'                 => 'required|numeric',
+            'password'              => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required'
+        ];
+
+        $pesan = [
+            'token.required'                      => 'Kode rahasia wajib di isi.',
+            'token.numeric'                       => 'Kode rahasia yang anda masukan salah!',
+            'password.required'                   => 'Password wajib di isi.',
+            'password.string'                     => 'Password berupa kombinasi huruf dan angka.',
+            'password.min'                        => 'Password minimal 6 karakter.',
+            'password.confirmed'                  => 'Password tidak sama. Pastikan password yang Anda masukan sama.',
+            'password_confirmation.required'      => 'Field Konfirmasi Password wajib di isi.'
+        ];
+
+        $validasi = Validator::make($request->all(), $rules, $pesan);
+
+        if ($validasi->fails()) {
+            return response([
+                'error'   => true,
+                'message' => $validasi->errors()
+            ]);
+        }
+
+        $check = PasswordReset::where('token', md5($request->token))
+                ->first();
+
+        if (!$check) {
+            return response([
+                'error'   => true,
+                'message' => 'Gagal melakukan verifikasi. Silahkan melakukan verifikasi ulang.'
+            ]);
+        }
+
+        $user = User::where('email', $check->email)->first();
+        $user->password = Hash::make($request->password);
+        $user->update();
+
+        $check->delete();
+
+        return response([
+            'success' => true,
+            'message' => 'Berhasil melakukan verifikasi token dan memperbarui password.',
+            'data'    => $user,
+        ]);
+    }
+
+    // public function verifyForgetPassword(Request $request)
+    // {
+    //     $rules = [
+    //         'token' => 'required|numeric'
+    //     ];
+
+    //     $pesan = [
+    //         'token.required' => 'Kode rahasia wajib di isi.',
+    //         'token.numeric'  => 'Kode rahasia yang anda masukan salah!'
+    //     ];
+
+    //     $validasi = Validator::make($request->all(), $rules, $pesan);
+
+    //     $check = PasswordReset::where('token', md5($request->token))
+    //             ->first();
+
+    //     if (!$check) {
+    //         return response([
+    //             'error'   => true,
+    //             'message' => 'Gagal melakukan verifikasi. Silahkan melakukan verifikasi ulang.',
+    //             'data'    => $check,
+    //         ]);
+    //     }
+
+    //     return response([
+    //         'success' => true,
+    //         'message' => 'Berhasil melakukan verifikasi token. Silahkan masukan password baru yang ingin Anda gunakan.'
+    //     ]);
+    // }
+
+    protected function checkEmailUser($email)
+    {
+        $check = User::where('email', $email)->first();
+
+        if (!empty($check)) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ resources/views/forgetpassword.blade.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <title>
+  </title>
+  <!--[if !mso]><!-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--<![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
+    #outlook a {
+      padding: 0;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+
+    table,
+    td {
+      border-collapse: collapse;
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+
+    img {
+      border: 0;
+      height: auto;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+
+    p {
+      display: block;
+      margin: 13px 0;
+    }
+  </style>
+  <!--[if mso]>
+        <noscript>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        </noscript>
+        <![endif]-->
+  <!--[if lte mso 11]>
+        <style type="text/css">
+          .mj-outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+  </style>
+  <!--<![endif]-->
+  <style type="text/css">
+    @media only screen and (min-width:480px) {
+      .mj-column-per-66-66666666666666 {
+        width: 66.66666666666666% !important;
+        max-width: 66.66666666666666%;
+      }
+
+      .mj-column-per-33-33333333333333 {
+        width: 33.33333333333333% !important;
+        max-width: 33.33333333333333%;
+      }
+
+      .mj-column-per-100 {
+        width: 100% !important;
+        max-width: 100%;
+      }
+
+      .mj-column-per-50 {
+        width: 50% !important;
+        max-width: 50%;
+      }
+    }
+  </style>
+  <style media="screen and (min-width:480px)">
+    .moz-text-html .mj-column-per-66-66666666666666 {
+      width: 66.66666666666666% !important;
+      max-width: 66.66666666666666%;
+    }
+
+    .moz-text-html .mj-column-per-33-33333333333333 {
+      width: 33.33333333333333% !important;
+      max-width: 33.33333333333333%;
+    }
+
+    .moz-text-html .mj-column-per-100 {
+      width: 100% !important;
+      max-width: 100%;
+    }
+
+    .moz-text-html .mj-column-per-50 {
+      width: 50% !important;
+      max-width: 50%;
+    }
+  </style>
+  <style type="text/css">
+    @media only screen and (max-width:480px) {
+      table.mj-full-width-mobile {
+        width: 100% !important;
+      }
+
+      td.mj-full-width-mobile {
+        width: auto !important;
+      }
+    }
+  </style>
+</head>
+
+<body style="word-spacing:normal;background-color:#e5e7eb;">
+  <div style="background-color:#e5e7eb;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;margin-top:10px;">
+      <tbody>
+        <tr>
+          <td>
+            <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+            <div style="margin:0px auto;max-width:600px; background-color: #fff;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+                <tbody>
+                  <tr>
+                    <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+                      <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:399.99999999999994px;" ><![endif]-->
+                      <div class="mj-column-per-66-66666666666666 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                          <tbody>
+                            <tr>
+                              <td align="left" style="font-size:0px;padding:10px 25px;padding-top:0;padding-bottom:0px;word-break:break-word;">
+                                <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;font-weight:bolder;line-height:1;text-align:left;color:#1e3a8a;">
+                                  <center>
+                                    <img src="https://admin.filterpedia.co.id/storage/images/online_security.svg" width="50%" alt="FilterPedia.co.id"><br>
+                                    FILTERPEDIA
+                                  </center>
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!--[if mso | IE]></td></tr></table><![endif]-->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+      <tbody>
+        <tr>
+          <td>
+            <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" bgcolor="#ffffff" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+            <div style="margin:0px auto;max-width:600px; background-color: #fff;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+                <tbody>
+                  <tr>
+                    <td style="direction:ltr;font-size:0px;padding-bottom:20px;text-align:center;">
+                      <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:199.99999999999997px;" ><![endif]-->
+                      <div class="mj-column-per-66-66666666666666 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;background-color: #1e40af;margin:0;" width="100%">
+                          <tbody>
+                            <tr>
+                              <td align="center" style="font-size:0px;padding-top:10px;padding-bottom:10px;word-break:break-word;">
+                                <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#fff;font-weight:bolder;">
+                                  PERMINTAAN RESET PASSWORD
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                      <!--[if mso | IE]></td></tr></table><![endif]-->
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!--[if mso | IE]></td></tr></table><![endif]-->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;margin:10px 0">
+      <tbody>
+        <tr>
+          <td>
+            <!--[if mso | IE]><v:rect style="mso-width-percent:1000;" xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false"><v:fill origin="0, -0.5" position="0, -0.5" src="http://191n.mj.am/img/191n/1t/h0.jpg" type="frame" size="1,1" aspect="atleast" /><v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0"><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+            <div style="margin:0px auto;max-width:600px;">
+              <div style="line-height:0;font-size:0;">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;background-color: #fff;">
+                  <tbody>
+                    <tr>
+                      <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:left;">
+                        <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                        <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                          <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                            <tbody>
+                              <tr>
+                                <td align="center" style="font-size:0px;padding:0 25px;padding-bottom:10px;word-break:break-word;">
+                                  <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:left;color:#45474e;">Hi <b>{{ $data->name }}</b>!</div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:10px;word-break:break-word;">
+                                  <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1.2;text-align:left;color:#45474e;">
+                                    Mohon maaf mengganggu kenyamanan Anda, namun sepertinya kami harus melakukan verifikasi terhadap permintaan perubahan password dari feature "Forget Password" pada website kami <a href="https://www.filterpedia.co.id" style="text-decoration:none;color:#1e3a8a;font-weight:bolder;">www.FilterPedia.co.id</a> pada tanggal <b>{{ date('Y-m-d') }}</b>, jam <b>{{ date('H:i:s') }}</b>. <br /><br />
+                                    Apabila Anda merasa ini bukan lah Anda, mohon untuk menghiraukan email ini. Namun apabila Anda memang melakukan tindakan ini, silahkan masukan 6 digit kode yang tertera di bawah ini untuk memverifikasi bahwa tindakan ini adalah tindakan yang Anda lakukan.
+                                  </div>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                        <!--[if mso | IE]></td></tr></table><![endif]-->
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <!--[if mso | IE]></td></tr></table></v:textbox></v:rect><![endif]-->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;margin:10px 0">
+      <tbody>
+        <tr>
+          <td>
+            <!--[if mso | IE]><v:rect style="mso-width-percent:1000;" xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false"><v:fill origin="0, -0.5" position="0, -0.5" src="http://191n.mj.am/img/191n/1t/h0.jpg" type="frame" size="1,1" aspect="atleast" /><v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0"><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+            <div style="margin:0px auto;max-width:600px;">
+              <div style="line-height:0;font-size:0;">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;background-color: #fff;">
+                  <tbody>
+                    <tr>
+                      <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:left;">
+                        <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                        <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                          <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                            <tbody>
+                              <tr>
+                                <td align="center" style="font-size:0px;padding:0 25px;padding-bottom:10px;word-break:break-word;">
+                                  <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;color:#45474e;margin-bottom:5px;">
+                                    Kode :
+                                  </div>
+                                  <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:24px;line-height:1;letter-spacing:5px;color:#1e3a8a;">
+                                    <b>{{ $data->kode_rahasia }}</b>
+                                  </div>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                        <!--[if mso | IE]></td></tr></table><![endif]-->
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <!--[if mso | IE]></td></tr></table></v:textbox></v:rect><![endif]-->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;margin:0px;">
+      <tbody>
+        <tr>
+          <td>
+            <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+            <div style="margin:0px auto;max-width:600px;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;background-color:#fff;">
+                <tbody>
+                  <tr>
+                    <td style="direction:ltr;padding:20px 0;text-align:center;">
+                      <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                      <div class="mj-column-per-100 mj-outlook-group-fix" style="text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                        <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:middle;" width="100%">
+                          <tbody>
+                            <tr>
+                              <td align="center" style="padding:10px 25px;padding-top:0px;padding-bottom:0px;word-break:break-word;">
+                                <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;line-height:1;text-align:center;color:#45474e;font-weight:bold;">PT. Cipta Aneka Air (CAA)
+                                </div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td align="center" style="padding:10px 25px;padding-top:0px;padding-bottom:0px;word-break:break-word;">
+                                <div style="padding:10px;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:14px;text-align:center;color:#45474e;">Kawasan Pergudangan Arcadia, Blok G18 No 1-3,<br />Jl. Daan Mogot, RT.005/RW.003, Batuceper, Kec. Batuceper.<br />Kota Tangerang, Banten - 15122
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                      <!--[if mso | IE]></td></tr></table><![endif]-->
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!--[if mso | IE]></td></tr></table><![endif]-->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
