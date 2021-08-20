16cad5334059ebff13bfbc34b0930fd2eb7c03a2
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-08-20 15:18:23 +0700

    Percobaan mengganti ID G. Analytics Universal agar dapat menarik Data.

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -84,5 +84,15 @@
             }
         </script>
         @yield('jq-script')
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205495987-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-205495987-1');
+        </script>
+
     </body>
 </html>
