e2bdd935c9361bb25f34198007d55beb5eae1137
ridwan zan haqqi <ridwan.haqi@gmail.com>
2021-10-13 15:26:53 +0700

    Perubahan

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
         $produk = Product::where('status', '!=', 9)
                     ->orderBy('id', 'DESC')
                     ->with('Discount')
-                    ->get();
+                    ->paginate(8);
 
         if ($produk->count() > 0) {
 
